f47b1e4abf0d1b58ce2d79277c00772edc3afc66
bitsydarel <bitsydarel@gmail.com>
2019-08-08 02:57:11 +0300

    Updated page route documentation and example to match the documentation.

--- example/dbpage_routing_example.dart
+++ example/dbpage_routing_example.dart
@@ -2,5 +2,13 @@ import "package:dbpage_routing/dbpage_routing.dart";
 
 dynamic main() {
   final awesome = const PageRoutePath("/");
-  print("awesome: $awesome");
+
+  print("awesome without arguments: $awesome");
+
+  final awesomeWithArguments = const PageRoutePath(
+    "/?username?password",
+    arguments: {"username": String, "password": String},
+  );
+
+  print("awesome with arguments: $awesomeWithArguments");
 }
--- lib/src/page_route_path.dart
+++ lib/src/page_route_path.dart
@@ -4,9 +4,19 @@ import "package:meta/meta.dart" show literal;
 ///
 /// Contains information about the location of page and his parameters.
 class PageRoutePath {
-  /// Used to annotated a Widget `a`.
+  /// Used to annotated a Widget `X`.
   ///
   /// Indicates the [path] of widget and the [arguments] the widget depends on.
+  ///
+  /// The [path] to required to access tbe widget, example:
+  /// "/randomPath"
+  ///
+  /// The [path] would contains the [arguments] name in it, example:
+  /// "/somePath?argument1?argument2"
+  ///
+  /// The [arguments] map has key as argument's name and value as it's type:
+  ///
+  /// {"argument1": String, "argument2": String}
   @literal
   const PageRoutePath(this.path, {this.arguments});
 
